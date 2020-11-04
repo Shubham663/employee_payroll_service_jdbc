2c1b9b1cb06c80c3e82df7f9bb290dcec9c49382
unknown <mittalshubham05@gmail.com>
2020-11-04 22:05:13 +0530

    Add: added functionality for adding multiple employees to json server using multithreading

---
+++ src/main/java/com/bridgelabz/employee_payroll_service_jdbc/PayrollJsonServerService.java
@@ -0,0 +1,66 @@
+package com.bridgelabz.employee_payroll_service_jdbc;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class PayrollJsonServerService {
+	private static PayrollJsonServerService paJsService;
+	Logger logger;
+
+	private PayrollJsonServerService() {
+		logger = LogManager.getLogger();
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 3000;
+	}
+
+	public static PayrollJsonServerService getInstance() {
+		if(paJsService == null)
+			paJsService = new PayrollJsonServerService();
+		return paJsService;
+	}
+
+	public synchronized Response addEmployeeToJsonServer(Employees employees) {
+		Response response = RestAssured.given()
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.body("{\"name\": \""+employees.getName()+"\",\"salary\": \""+ employees.getSalary() + "\",\"id\": " + employees.getEmployeeID() + "}")
+				.when()
+				.post("/employees/create");
+
+		return response;
+	}
+
+	public Boolean addMultipleEmployeesToJsonServer(List<Employees> list) {
+		Response response = null;
+		Map<Integer,Boolean> employeeAddStatus = new HashMap<>();
+		list.forEach(employee -> {
+			Runnable task = () -> {
+				employeeAddStatus.put(employee.hashCode(), false);
+				final Response response2 = addEmployeeToJsonServer(employee);
+				
+				employeeAddStatus.put(employee.hashCode(), true);
+			};
+			Thread thread = new Thread(task,employee.getName());
+			thread.start();
+		});
+		while(employeeAddStatus.size()<list.size() || employeeAddStatus.containsValue(false)) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				logger.error("Error while waiting for threads to finish " + e.getMessage());
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+}
--- src/test/java/com/bridgelabz/employee_payroll_service_jdbc/EmployeeJsonServerTest.java
+++ src/test/java/com/bridgelabz/employee_payroll_service_jdbc/EmployeeJsonServerTest.java
@@ -1,8 +1,13 @@
 package com.bridgelabz.employee_payroll_service_jdbc;
 
+import static org.junit.Assert.assertThat;
+
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hamcrest.Matchers;
+import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -30,20 +35,27 @@ public class EmployeeJsonServerTest {
 	public void addEmployeeToJsonServerTest() {
 		Date date = Date.valueOf("2020-05-01");
 		Employees employees = new Employees("Harleen Kaur", 15,20000, date ,"Female",20000,10000,10000,1000,19000,9878767651l);
-		
-		Response response = RestAssured.given()
-								.contentType(ContentType.JSON)
-								.accept(ContentType.JSON)
-								.body("{\"name\": \"Harleen Kaur\",\"salary\": \"20000\"}")
-								.when()
-								.post("/employees/create");
-		
+		PayrollJsonServerService paService = PayrollJsonServerService.getInstance();
+		Response response = paService.addEmployeeToJsonServer(employees);
 		String responseAsString = response.asString();
 		System.out.println(responseAsString);
 		JsonObject jsonObject = new Gson().fromJson(responseAsString, JsonObject.class);
 		int id = jsonObject.get("id").getAsInt();
 		response.then().body("id", Matchers.any(Integer.class));
 		response.then().body("name", Matchers.is("Harleen Kaur"));
-		response.then().body("salary",Matchers.is("20000"));
+		response.then().body("salary",Matchers.is("20000.0"));
+	}
+	
+	@Test
+	public void addMultipleEmployeesToJsonServerTest_returnsTrueWhenAdditionSuccessful() {
+		Date date = Date.valueOf("2020-05-01");
+		Employees employees = new Employees("Harleen Kaur", 15,20000, date ,"Female",20000,10000,10000,1000,19000,9878767651l);
+		Employees employees2 = new Employees("Simranjeet", 10, 30000, date, "Male",30000 , 10000, 20000, 2000, 28000, 9979876251l);
+		List<Employees> list = new ArrayList<>();
+		list.add(employees);
+		list.add(employees2);
+		PayrollJsonServerService paService = PayrollJsonServerService.getInstance();
+		Boolean boolean1 = paService.addMultipleEmployeesToJsonServer(list);
+		assertThat("Employees not added", true, Is.is(true));
 	}
 }
