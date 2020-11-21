ba3e3b6f80c67e1f07de71a5f37bff99e68d10d8
unknown <mittalshubham05@gmail.com>
2020-11-21 12:08:42 +0530

    Edit: created callback functions for methods get,post and delete

--- ajax-manipulation.js
+++ ajax-manipulation.js
@@ -1,11 +1,11 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-function makeAjaxCall(methodType,url,async=true,data=null){
+function makeAjaxCall(methodType,url,callback,async=true,data=null){
     let xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function(){
         if(xhr.readyState == 4){
             if(xhr.status == 200 || xhr.status == 201)
-                console.log("The response obtained is " + xhr.responseText);
+                callback("The response obtained is " + xhr.responseText);
             else
                 console.log("Need for handling 400/500 i.e. Client/Server Error");
         }
@@ -20,12 +20,20 @@ function makeAjaxCall(methodType,url,async=true,data=null){
 }
 
 const getURL = "http://localhost:3000/employees/2";
-console.log("The User data as obtained through ajax call on url " + getURL);
-makeAjaxCall("GET",getURL);
+const getUserDetails = (data) => {
+    console.log("The user details as got from get call: " + data);
+};
+makeAjaxCall("GET",getURL,getUserDetails);
 
 const postURL = "http://localhost:3000/employees/";
 const empData = {"name": "Harry","salary":"5000","id":4};
-makeAjaxCall("POST",postURL,false,empData);
+const userAdded = (data) => {
+    console.log("The user details added through post call: " + data);
+};
+makeAjaxCall("POST",postURL,userAdded,false,empData);
 
 const deleteURL = "http://localhost:3000/employees/4"
-makeAjaxCall("DELETE",deleteURL,true);
+const userDeleted = (data) => {
+    console.log("The user details deleted through delete call: " + data);
+};
+makeAjaxCall("DELETE",deleteURL,userDeleted,true);
